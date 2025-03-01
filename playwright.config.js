a4a54dbe5a3772f07823b14b8e954ea968f4e1a6
OlegArtsy <glutamin97@gmail.com>
2025-02-28 18:08:24 -0600

    commit reports change

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['junit', { outputFile: 'test-results/junit-report.xml' }],
-    ['html']
+    ['junit', { outputFile: 'junit-results.xml' }],
+    ['html', { outputFolder: 'playwright-report' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
