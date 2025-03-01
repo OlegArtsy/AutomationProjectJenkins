685e216ee81c7ae3a02edb336037818575ce4684
OlegArtsy <glutamin97@gmail.com>
2025-02-28 18:12:45 -0600

    folder update

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['junit', { outputFile: 'junit-results.xml' }],
-    ['html', { outputFolder: 'playwright-report' }]
+    ['html', { outputFolder: 'playwright-reports' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
